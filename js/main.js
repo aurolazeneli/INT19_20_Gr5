dbc22479ffc8e8346c32c1b73c9a3319bf902282
aurolazeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-01-03 17:43:57 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function validate1()
+{
+  var username = document.getElementById('name');
+  var useremail = document.getElementById('email');
+  if(username.value.trim() =="" || email.value.trim() =="")
+  {
+    alert("No blank values allowed");
+    return false;
+  }
+  else
+  {
+  true;
+  }
+}
+const input_fields = {
+name: 	/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$/,
+
+email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+}
+const validate = (field, regex) => {
+regex.test(field.value) ? field.className = 'valid' : field.className = 'invalid';
+
+}
+let keys = document.querySelectorAll('input');
+keys.forEach(item => item.addEventListener(
+'keyup', e => {
+  validate(e.target, input_fields[e.target.attributes.name.value])
+}
+));
