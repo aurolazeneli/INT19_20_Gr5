231e0b800b5d0d4a3211590594a4ed0c33e36720
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-01-04 23:56:38 +0100

    Update fashion.js

--- fashion.js
+++ fashion.js
@@ -28,3 +28,23 @@ keys.forEach(item => item.addEventListener(
   validate(e.target, input_fields[e.target.attributes.name.value])
 }
 ));
+function myFunctionm() {
+  var str = "The rain in SPAIN stays mainly in the plain";
+  var res = str.match(/ain/g);
+  document.getElementById("demo").innerHTML = res;
+}
+
+function Person(first, last, age, color) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = color;
+}
+
+var diorProduct = new Person("Christian ", "Dior", 1905 , "blue");
+var hudaProduct = new Person("Huda", "Kattan", 1983, "green");
+
+document.getElementById("Pdemo").innerHTML =
+"Christian Dior made dior product on  " + diorProduct.age + ".\n Huda Kattan made huda product on  " + hudaProduct.age;
+
+
