28c6c99f7d334822b0ccbf925137dfca780ea187
aurolazeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-01-05 18:00:07 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,38 +1,35 @@
-function validate1()
-{
-  var uname = document.getElementById('uname');
-  var pwd = document.getElementById('pwd');
-  if(uname.value.trim() =="" || pwd.value.trim() =="")
-  {
-    alert("Both fields should not be blank");
-    return false;
-  }
-  else
-  {
-  true;
-}
-/*if(pwd.length<6|| pwd>12){
-  alert("Password must be 6 to 12 characters");
-  return;
-}
-var str=uname.slice(0,1);
-if(uname.slice(0,1)=="_" ||uname.slice(0,1)=="@" || str.match(/[0-9]/g)!null){
-  alert("username should not start with _, @ and Number");
-  return;
-}*/
-}
-/*const input_fields = {
-name: 	/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$/,
 
-email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+var attempt = 5; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("l_username").value;
+var password = document.getElementById("l_userpassword").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
 }
-const validate = (field, regex) => {
-regex.test(field.value) ? field.className = 'valid' : field.className = 'invalid';
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("l_username").disabled = true;
+document.getElementById("l_userpassword").disabled = true;
+document.getElementById("btn").disabled = true;
+return false;
+}
+}
+}
 
-}
-let keys = document.querySelectorAll('input');
-keys.forEach(item => item.addEventListener(
-'keyup', e => {
-  validate(e.target, input_fields[e.target.attributes.name.value])
-}
-));*/
+
+$(document).ready(function(){
+
+  $("input").focus(function(){
+    $(this).css("background-color", "");
+
+  });
+  $("input").blur(function(){
+    $(this).css("background-color","");
+  });
+});
