87cf99f9816b149dd8e72c29f27534ee7a1b1123
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-01-05 18:45:12 +0100

    Update makeup.js

--- makeup.js
+++ makeup.js
@@ -68,3 +68,27 @@ function filter() {
     }
   }
 }
+
+function initStorage() {
+
+function saveName() {
+  var name = document.getElementById('name');
+  localStorage.setItem('name',name.value);
+  
+}
+function saveEmail() {
+var email = document.getElementById('email');
+  localStorage.setItem('email',email.value);
+
+}
+saveName();
+saveEmail();
+
+
+}
+
+function clearAll() {
+  localStorage.clear();
+  window.location.reload();
+
+}
