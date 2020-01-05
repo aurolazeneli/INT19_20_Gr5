55261ca278131b4f161424e8acb553bb6035aac8
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-01-05 16:27:10 +0100

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,3 +1,22 @@
+
+function validate1()
+{
+  var username = document.getElementById('name');
+  var surname = document.getElementById('surname');
+  var useremail = document.getElementById('email');
+  var mobphone =document.getElementById('phone');
+
+  if(username.value.trim() =="" || email.value.trim() =="")
+  {
+    alert("No blank values allowed");
+    return false;
+  }
+  else
+  {
+  true;
+  }
+}
+
 const input_fields = {
 name: 	/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$/,
 Surname:	/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]*$/,
@@ -14,3 +33,44 @@ keys.forEach(item => item.addEventListener(
   validate(e.target, input_fields[e.target.attributes.name.value])
 }
 ));
+function showPosition() {
+
+
+       if(navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(showMap, showError);
+       }
+       else {
+        alert("Sorry, your browser does not support HTML5 geolocation.");
+    }
+    }
+
+    function showMap(position) {
+
+      lat = position.coords.latitude;
+    long = position.coords.longitude;
+    var latlong = new google.maps.LatLng(lat, long);
+
+
+
+     var myOptions = {
+        center: latlong,
+        zoom: 16,
+        mapTypeControl: true,
+        navigationControlOptions: {
+            style:google.maps.NavigationControlStyle.SMALL
+        }
+    }
+     var map = new google.maps.Map(document.getElementById("embedMap"), myOptions);
+    var marker = new google.maps.Marker({ position:latlong, map:map, title:"You are here!" });
+}
+function showError(error) {
+    if(error.code == 1) {
+        result.innerHTML = "You've decided not to share your position, but it's OK. We won't ask you again.";
+    } else if(error.code == 2) {
+        result.innerHTML = "The network is down or the positioning service can't be reached.";
+    } else if(error.code == 3) {
+        result.innerHTML = "The attempt timed out before it could get the location data.";
+    } else {
+        result.innerHTML = "Geolocation failed due to unknown error.";
+    }
+}
